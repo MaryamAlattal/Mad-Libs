0ab2a5879614690d50c00ff68423733a3514b3ec
MaryamAlattal <110087526+MaryamAlattal@users.noreply.github.com>
2023-09-07 23:57:09 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+#Building A Mad Libs Game
+colour = input("Enter a colour: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are", colour)
+print(plural_noun + " are blue")
+print("I love", celebrity)
